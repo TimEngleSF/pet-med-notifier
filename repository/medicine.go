6e2a78f5d1afa08205c9c3542cf691755d2087c9
Tim Engle <timothy.englew@gmail.com>
2024-03-01 23:25:47 -0800

    Added GetDailyMedicines db query

--- repository/medicine.go
+++ repository/medicine.go
@@ -1,23 +1,61 @@
 package repository
 
 import (
+	"context"
 	"time"
 
-	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+const TimeZone = "America/Los_Angeles"
+
 type TimeMed struct {
-	Hour int
-	Min  int
+	Hour int `bson:"hour"`
+	Min  int `bson:"min"`
 }
 
 type Medicine struct {
-	Name       string
-	TimeToTake *TimeMed
-	Taken      bool
-	TimeTaken  *TimeMed
-	Date       *time.DateOnly
+	Name       string     `bson:"name"`
+	TimeToTake *TimeMed   `bson:"time-to-take"`
+	Taken      bool       `bson:"taken"`
+	TimeTaken  *TimeMed   `bson:"time-taken"`
+	Date       *time.Time `bson:"date"`
 }
 
-func (m *Medicine) CreateMedicine(c echo.Context, db mongo.Database) error {}
+// func (m *Medicine) CreateMedicine(c echo.Context, db mongo.Database) error {}
+
+func GetDailyMedicines(c context.Context, d mongo.Database) ([]Medicine, error) {
+	collection := d.Collection("medicines")
+
+	loc, err := time.LoadLocation(TimeZone)
+	if err != nil {
+		return nil, err
+	}
+
+	today := time.Now().In(loc).Truncate(24 * time.Hour)
+
+	filter := bson.M{"date": bson.M{"$gt": today}}
+
+	var results []Medicine
+	cursor, err := collection.Find(c, filter)
+
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(c)
+
+	for cursor.Next(c) {
+		var medicine Medicine
+		if err := cursor.Decode(&medicine); err != nil {
+			return nil, err
+		}
+		results = append(results, medicine)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	return results, nil
+}
